b949647f75f44b648636d1bf69d9d33da9bc5271
Alexandre Di Guida <alexandre.diguida@gmail.com>
2017-02-26 23:42:11 +0000

    project setup - created main and navigation component

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var Nav = require('Nav');
+
+// Will be responsible for rendering our individuals pages
+var Main = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <Nav/>
+        <h2>Main Component</h2>
+      </div>
+    );
+  }
+});
+
+module.exports = Main;
---
+++ app/components/Nav.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+// Top naavigation bar
+var Nav = React.createClass({
+  render: function () {
+    return (
+      <h2>TODO: Nav Component</h2>
+    );
+  }
+});
+
+module.exports = Nav;
