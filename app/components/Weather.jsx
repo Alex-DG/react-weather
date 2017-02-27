8866e06ee4084bff7a12fe18b324448acebc80ea
Alexandre Di Guida <alexandre.diguida@gmail.com>
2017-02-27 10:30:42 +0000

    app navigation structure done - with router route index route and link

---
+++ app/components/Weather.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Weather = React.createClass({
+  render: function () {
+    return (
+      <h3>TODO: Weather Component</h3>
+    );
+  }
+});
+
+module.exports = Weather;
